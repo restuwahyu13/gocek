91135269301e8e96698219db100c8498b4f244e4
pazemo <pazemo@pazemos-MacBook-Air.local>
2022-12-16 10:02:05 +0700

    Add more validation for ToBeZero, ToBeMinus, ToBeGreaterThan | Eq, TobeLessThan | eq

--- ToBeZero.go
+++ ToBeZero.go
@@ -12,7 +12,7 @@ func (h *gocekAssertion) ToBeZero() {
 	if !reflect.DeepEqual(input, nil) {
 		inputType := reflect.TypeOf(input).Kind().String()
 
-		notObjectType := inputType == reflect.Slice.String() || inputType == reflect.Array.String() || inputType == reflect.Map.String() || inputType == reflect.Bool.String() || inputType == reflect.Func.String() || inputType == reflect.Chan.String() || inputType == reflect.Pointer.String() || inputType == reflect.Struct.String() || inputType == reflect.Complex64.String() || inputType == reflect.Complex128.String() || inputType == reflect.String.String() || inputType == reflect.Ptr.String()
+		notObjectType := inputType == reflect.Slice.String() || inputType == reflect.Array.String() || inputType == reflect.Map.String() || inputType == reflect.Bool.String() || inputType == reflect.Func.String() || inputType == reflect.Chan.String() || inputType == reflect.Pointer.String() || inputType == reflect.Struct.String() || inputType == reflect.Complex64.String() || inputType == reflect.Complex128.String() || inputType == reflect.String.String() || inputType == reflect.Ptr.String() || inputType == reflect.Interface.String() || inputType == reflect.Invalid.String() || inputType == reflect.Uintptr.String()
 
 		if h.o == "==" && notObjectType {
 			h.t.FailNow()
